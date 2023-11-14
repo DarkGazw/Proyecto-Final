52ec536f77f2eb31ca5ca68ee0b0fa5045f7f1a1
DarkGazw <gazce44@gmail.com>
2023-11-13 21:03:05 -0300

    base de datos

--- gestion_internaciones/models.py
+++ gestion_internaciones/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
 # Create your models here.
+class Pacientes(models.Model):
+    nombre_pac = models.CharField(max_length=30)
+    apellido_pac = models.CharField(max_length=15)
+    dni_pac = models.CharField(max_length=10)
+    domicilio_pac = models.CharField(max_length=50)
+    telefono_pac = models.CharField(max_length=12)
+    estado_pac = ('Internado', 'Alta', 'Fallecido')
+
+    
+class Habitaciones(models.Model):
+    ubicacion_hab = models.CharField(max_length=50)
+    camas_hab = models.IntegerField()
+    camasdispo_hab = models.IntegerField()
+
+class Internaciones(models.Model):
+    paciente_inter = models.ForeignKey("Pacientes", on_delete=models.CASCADE)
+    habitacion_inter = models.ForeignKey("Habitaciones", on_delete=models.CASCADE)
+    fecha_ingreso = models.DateField(auto_now_add=True)
+    hora_ingreso = models.TimeField(auto_now_add=True)
+    fecha_egreso = models.DateField(auto_now=True)
+    hora_egreso = models.TimeField(auto_now=True)
+
+class Cargos(models.Model):
+    nombre_car = models.CharField(max_length=50)
+
+class Personal(models.Model):
+    nombre_per = models.CharField(max_length=30)
+    apellido_per = models.CharField(max_length=15)
+    dni_per = models.CharField(max_length=10)
+    domicilio_per = models.CharField(max_length=50)
+    telefono_per = models.CharField(max_length=12)
+    cargo = models.ForeignKey("Cargos",on_delete=models.CASCADE)
+
+class Obras_Sociales(models.Model):
+    nombre_obra = models.CharField(max_length=10)
+    tipo = models.CharField(max_length=10)
+    prestaciones = models.TextField()
+
+class Coseguros(models.Model):
+    nombre_cos = models.CharField(max_length=10)
+    tipo = models.CharField(max_length=10)
+    prestaciones = models.TextField()
+
+class Obras_Pacientes(models.Model):
+    paciente = models.ForeignKey("Pacientes",on_delete=models.CASCADE)
+    obra_social = models.ForeignKey("Obras_Sociales",on_delete=models.CASCADE)
+    coseguro = models.ForeignKey("Coseguros",on_delete=models.CASCADE)
+
+class Personal_Paciente(models.Model):
+    personal_a_cargo = models.ForeignKey("Personal",on_delete=models.CASCADE)
+    paciente = models.ForeignKey("Pacientes",on_delete=models.CASCADE)
+
+class Drogueria(models.Model):
+    nombre_drog = models.CharField(max_length=20)
+    stock_drog = models.IntegerField()
+
+class Prescripciones(models.Model):
+    personal_paciente = models.ForeignKey("Personal_Paciente", on_delete=models.CASCADE)
+    medicamentos = models.ForeignKey("Drogueria", on_delete=models.CASCADE)
+    detalles_de_control = models.TextField()
