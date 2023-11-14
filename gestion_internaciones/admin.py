7dd89cc559f9405e4993a47d2ad63909fc892335
DarkGazw <gazce44@gmail.com>
2023-11-13 21:19:40 -0300

    admin

--- gestion_internaciones/admin.py
+++ gestion_internaciones/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from gestion_internaciones.models import Pacientes, Drogueria, Personal, Cargos, Obras_Sociales, Coseguros
 # Register your models here.
+class agregarpacientes(admin.ModelAdmin):
+    pass 
+
+class agregardrogas(admin.ModelAdmin):
+    pass
+
+class agregarcargo(admin.ModelAdmin):
+    pass
+
+class agregarpersonal(admin.ModelAdmin):
+    pass
+
+class agregarobrasocial(admin.ModelAdmin):
+    pass
+
+class agregarcoseguros(admin.ModelAdmin):
+    pass
+
+admin.site.register(Pacientes)
+admin.site.register(Drogueria)
+admin.site.register(Cargos)
+admin.site.register(Personal)
+admin.site.register(Obras_Sociales)
+admin.site.register(Coseguros)
--- gestion_internaciones/urls.py
+++ gestion_internaciones/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from gestion_internaciones import views
 
 urlpatterns = [
-    
+    path('pacientes/', views.listapacientes, name='listapacientes')
 ]
